1adc41abe47613fc42bc33d794afc002b2f23267
Eddie Madrigal <edmadrigal@yahoo.com>
2020-03-10 17:57:50 -0700

    reactstrap styling added

--- github-users/src/App.js
+++ github-users/src/App.js
@@ -8,43 +8,58 @@ import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
-
   state = {
     users: [],
-    usersSearch: ""
+    searchText: '',
+    followers: []
   };
-  
 
   componentDidMount() {
     axios
-    .get('https://api.github.com/users/eddiemadrigal')
-    .then (res => {
-      // console.log("Data: ", res.data);
-      this.setState({
-        users: res.data
-      });
-      console.log("Users array: ", this.state.users)
-    })
-    .catch (err => console.log(err));
+      .get('https://api.github.com/users/eddiemadrigal')
+      .then(res => {
+        this.setState({
+          users: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.users !== this.state.users) {
+      console.log('new users array');
+      if (this.state.searchText === 'chihuahua') {
+        console.log(this.state.searchText);
+        axios
+          .get('https://api.github.com/users/eddiemadrigal')
+          .then(res => {
+            this.setState({
+              users: res.data.message,
+              searchText: 'husky'
+            });
+          })
+          .catch(err => console.log(err.message));
+      }
+    }
   }
 
   handleChanges = e => {
     this.setState({
-      usersSearch: e.target.value
+      searchText: e.target.value
     });
   };
 
   fetchUsers = e => {
     e.preventDefault();
     axios
-    .get(`https://api.github.com/users/${this.state.usersSearch}`)
-    .then( res => {
-      this.setState ({
-        users: res.data
-      });
-    })
-    .catch(err => console.log('Error: ', err));
-  }
+      .get(`https://api.github.com/users/${this.state.searchText}`)
+      .then(res => {
+        this.setState({
+          users: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  };
 
   render() {
     return (
@@ -55,7 +70,7 @@ class App extends React.Component {
               <h1>GitHub User Search</h1>
                 <Row form>
                   <Col md={{ size: 9 }}>
-                    <Input placeholder="GitHub User Name" value = { this.state.usersSearch }  onChange = { this.handleChanges } />
+                    <Input placeholder="GitHub User Name" value = { this.state.searchText }  onChange = { this.handleChanges } />
                   </Col>
                   <Col md={{ size: 3 }}>
                     <Button color="success" onClick = { this.fetchUsers }>Search</Button>
@@ -66,17 +81,39 @@ class App extends React.Component {
                 <CardBody>
                   <CardTitle><h2>{ this.state.users.name }</h2></CardTitle>
                   <CardSubtitle><b>Login: { this.state.users.login }</b></CardSubtitle>
-                  <CardText>Bio: { this.state.users.bio }</CardText>
+                  <CardText className="bio-info"><b>Bio:</b> { this.state.users.bio }</CardText>
+                  <CardText className="bio-info"><b>Followers:</b> { this.state.users.followers }</CardText> 
+                  <CardText className="bio-info"><b>Following:</b> { this.state.users.following }</CardText>
                   <Button href={ this.state.users.html_url } target="_blank" color="primary">Learn More</Button>
                 </CardBody>
               </Card>
             </Col>
           </Row>
+
+          <Row>
+            <Col sm="12" md={{ size: 6, offset: 3 }}>
+              <h2>Followers</h2>
+              
+                {this.state.followers.map( follower => (
+
+                  <Card className="card-info" key={ follower.id }>
+                    <CardImg top width="100%" src={ follower.avatar_url } alt="GitHub profile pic" />
+                    <CardBody>
+                      <CardTitle><h2>{ follower.name }</h2></CardTitle>
+                      <CardSubtitle><b>GitHub User ID: { follower.login }</b></CardSubtitle><br />
+                      <Button href={ follower.html_url } target="_blank" color="primary">Learn More</Button>
+                    </CardBody>
+                  </Card>
+
+                ))}
+              
+            </Col>
+          </Row>
+
         </Container>
       </div>
-    )
+    );
   }
-
 }
 
 export default App;
